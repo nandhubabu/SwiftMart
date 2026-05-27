553a08e8e0ed748d50dc670c6918c957b4538030
nandhubabu <nandhubabuvktd@gmail.com>
2026-05-28 00:34:54 +0530

    feat: initialize server with Express, Mongoose connection, and environment configuration

--- server/server.js
+++ server/server.js
@@ -8,10 +8,20 @@ app.get('/', (req, res) => {
     res.send("Hello from home");
 })
 
+
+
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected"))
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 
+const product_Schema = new mongoose.Schema({
+    name: { type: String, required: true },
+    prize: { type: Number, required: true },
+    description: String,
+    img_url: { type: String, required: true },
+    seller_id: { type: String, required: true }
+})
+const Product = mongoose.model('Product', product_Schema);
 app.listen(port, () => {
     console.log(`server is running on http://localhost:${port}`);
 });
