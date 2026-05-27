dab29515fcfc96677e979b7cc511e999e81d4105
nandhubabu <nandhubabuvktd@gmail.com>
2026-05-28 00:14:57 +0530

    feat: initialize server project with express, mongoose, and dotenv configuration

--- server/server.js
+++ server/server.js
@@ -2,19 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const port = 5000;
+require('dotenv').config()
 
 app.get('/', (req, res) => {
     res.send("Hello from home");
 })
 
-mongoose.connect('mongodb://localhost:27017/swiftmart')
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((err) => {
-        console.log('Error connecting to MongoDB', err);
-    })
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log(err));
 
 app.listen(port, () => {
     console.log(`server is running on http://localhost:${port}`);
-})
+});
