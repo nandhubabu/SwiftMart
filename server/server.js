7fc587950a50054782f66772bd3b73d750571abd
nandhubabu <nandhubabuvktd@gmail.com>
2026-05-27 23:41:50 +0530

    chore: initialize node_modules for mongodb and mongoose dependencies

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-    res.send("Helllo")
+    res.send("Hello from home");
 })
 
+mongoose.connect('mongodb://localhost:27017/swiftmart')
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log('Error connecting to MongoDB', err);
+    })
+
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
+    console.log(`server is running on http://localhost:${port}`);
 })
