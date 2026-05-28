dcc02f175264fc198a7b31a52acfb792e80c6e40
nandhubabu <nandhubabuvktd@gmail.com>
2026-05-28 12:57:18 +0530

    feat: initialize express server with MongoDB connection and product creation endpoint while updating gitignore configurations

--- server/server.js
+++ server/server.js
@@ -1,13 +1,8 @@
+require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const port = 5000;
-require('dotenv').config()
-
-app.get('/', (req, res) => {
-    res.send("Hello from home");
-})
-
+const port = process.env.PORT || 5000;
 
 
 mongoose.connect(process.env.MONGO_URI)
@@ -22,6 +17,24 @@ const product_Schema = new mongoose.Schema({
     seller_id: { type: String, required: true }
 })
 const Product = mongoose.model('Product', product_Schema);
+
+app.use(express.json());
+app.post('/api/products', async (req, res) => {
+    try {
+        const new_product = new Product(req.body);
+
+        const saved_product = await new_product.save();
+
+        res.status(201).json(saved_product);
+    } catch (error) {
+        res.status(201).json({ error: error.message });
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send("Hello from home");
+})
+
 app.listen(port, () => {
     console.log(`server is running on http://localhost:${port}`);
 });
